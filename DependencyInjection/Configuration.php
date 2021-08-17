f1a697d278848706b222dc42da779f82758f3a47
iznaurbeksultanov <ibeksultanov@iiko.ru>
2021-08-17 20:54:50 +0300

    update composer for new symfony version

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -31,7 +31,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('gearman');
         $rootNode = $treeBuilder->root('gearman');
 
         $rootNode
